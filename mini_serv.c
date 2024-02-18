a6023c838701ce423e326588265d97ad9d847f05
Picsart Academy <picsartacademy@PicsartAcademys-MacBook-Pro-33.local>
2024-02-18 18:29:49 +0400

    mini_serv

--- mini_serv.c
+++ mini_serv.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/select.h>
-
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <string.h>
 #include <unistd.h>
-#include <netdb.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/select.h>
+#include <netdb.h>
 
-
-
-typedef struct t_cl
-{
-	int id;
-	int fd;
-	struct t_cl* next;
-} clt;
-
-clt *clients = NULL;
-fd_set current, read_set, write_set;
-int sockfd, g_id;
-char msg[200000], buff[200000];
-
-
+int sockfd;
+int clients = 65000;
+int SIZE = 200000;
 
 void fatal( void )
 {
@@ -31,103 +19,101 @@ void fatal( void )
 	exit(1);
 }
 
-int get_max( void )
-{
-	int max = sockfd;
-	clt* tmp = clients;
-	while (tmp)
-	{
-		if (max > tmp->fd)
-			max = tmp->fd;
-		tmp = tmp->next;
-	}
-	return (max);
-}
 
-int getid(int fd)
-{
-	clt *tmp = clients;
-	while (tmp)
-	{
-		if (tmp->fd == fd)
-			return (tmp->id);
-		tmp = tmp->next;
-	}
-	return (-10);
-}
-
-void add( void )
-{
-	clt *tmp = clients;
-	clt *new;
-	int client_fd;
-
-	struct sockaddr_in claddr;
-	socklen_t len = sizeof(claddr);
-	client_fd = accept(sockfd, (struct sockaddr*)&claddr, &len);
-	if (client_fd < 0)
-		fatal();
-	memset(&new, 0, sizeof(new));
-	sprintf(buff, "server: client %d just arrived\n", g_id);
-	FD_SET(client_fd, &current);
-	new = malloc(sizeof(clt));
-	if (!new)
-		fatal();
-	new->fd = client_fd;
-	new->id = g_id++;
-	new->next = NULL;
-	if (!clients)
-		clients = new;
-	else
-	{
-		while (tmp->next)
-			tmp = tmp->next;
-		tmp->next = new;
-	}	
-}
-
-
-
-int main(int ac, char **av)
+int main(int ac, char** av)
 {
 	if (ac != 2)
 	{
 		write(2, "Wrong number of arguments\n", strlen("Wrong number of arguments\n"));
 		exit(1);
 	}
-	struct sockaddr_in srvaddr;
-	memset(&srvaddr, 0, sizeof(srvaddr));
-	srvaddr.sin_family = AF_INET;
-	srvaddr.sin_addr.s_addr = htonl(2130706433);
-	srvaddr.sin_port = htons(atoi(av[1]));
-
+	struct sockaddr_in srv;
+	fd_set active, ready, wr;
+	int id = 0;
+	int arr[clients];
+	char buffer[SIZE];
+	char msg[SIZE];
+	
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 		fatal();
-	if ((bind(sockfd, (struct sockaddr*)&srvaddr, sizeof(srvaddr)) < 0))
+	srv.sin_family = AF_INET;
+	srv.sin_addr.s_addr = htonl(2130706433);
+	srv.sin_port = htons(atoi(av[1]));
+	if (bind(sockfd, (struct sockaddr*)&srv, sizeof(srv)) < 0) 
 		fatal();
-	if ((listen(sockfd, 100) < 0))
+	if (listen(sockfd, 100))
 		fatal();
-	FD_ZERO(&current);
-	FD_SET(sockfd, &current);
-	memset(&msg, 0, sizeof(msg));
-
+	int maxfd = sockfd;
+	FD_ZERO(&active);
+	FD_SET(sockfd, &active);
 	while (1)
 	{
-		read_set = write_set = current;
-		if (select(get_max() + 1, &read_set, &write_set, NULL, NULL) < 0)
+		ready = wr = active;
+		if (select(maxfd + 1, &ready, &wr, NULL, NULL) < 0)
 			continue;
-		for(int fd = 0; fd <= get_max(); fd++)
+		for(int fd = 0; fd <= maxfd; fd++)
 		{
-			if (FD_ISSET(fd, &read_set))
+			if (FD_ISSET(fd, &ready))
 			{
+				bzero(&msg, strlen(msg));
+				bzero(&buffer, strlen(msg));
 				if (fd == sockfd)
 				{
-					add();
-					break;
+					//new client just arrived
+					struct sockaddr_in claddr;
+					socklen_t len = sizeof(claddr);
+					
+					int clt = accept(sockfd, (struct sockaddr*)&claddr, &len);
+					if (clt < 0)
+						continue;
+					maxfd = (clt > maxfd) ? clt : maxfd;
+					sprintf(buffer, "server: client %d just arrived\n", id);
+					arr[clt] = id++;
+					for(int j = 2; j <= maxfd; j++)
+					{
+						if (FD_ISSET(j, &wr) && j != sockfd)
+						{
+							if (send(j, buffer, strlen(buffer), 0) < 0)
+								fatal();
+						}
+					}
+					FD_SET(clt, &active);
 				}
+				else if (fd != sockfd)
+				{
+					//the client request
+					int count = 1;
+					while (count == 1 && msg[strlen(msg)-1] != '\n')
+						count = recv(fd, msg + strlen(msg), 1, 0);
+					if (count <= 0)
+					{
+						sprintf(buffer, "server: cleint %d just left", arr[fd]);
+						FD_CLR(fd, &active);
+						close(fd);
+						for(int z = 2; z <= maxfd; z++)
+						{
+							if (FD_ISSET(z, &wr) && z != fd)
+							{
+								if (send(z, buffer, strlen(buffer), 0) < 0)
+								{
+									fatal();
+								}
+							}
+						}
+					}
+					else
+					{
+						sprintf(buffer, "client %d: %s", arr[fd], msg);
+						for(int z = 2; z <= maxfd; z++)
+							if (FD_ISSET(z, &wr) && z != sockfd)
+								if (send(z, buffer, strlen(buffer), 0) < 0)
+									fatal();
+					}
+				}
+				FD_CLR(fd, &ready);
 			}
 		}
 	}
-	return (0);
+	return (0);	
 }
